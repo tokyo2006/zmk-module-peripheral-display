91add6e36a16cdd1afbd270e7bee0857a4cf2ef1
Chen Zeng <rurounikexin@gmail.com>
2026-08-31 22:50:21 +0800

    feat(display): debug line behind CONFIG_ZMK_PERIPHERAL_DISPLAY_DEBUG
    Renders a bottom-left line showing the last received status
    (layer/battery/flags/mods/wpm) plus a receive counter, so the peripheral
    can be diagnosed without any serial backend. Off by default.

--- src/peripheral_display.c
+++ src/peripheral_display.c
@@ -80,6 +80,11 @@ static struct zmk_widget_peripheral_bongo_cat bongo_cat_w;
 static lv_obj_t *no_link_label;
 static bool had_link;
 
+#if IS_ENABLED(CONFIG_ZMK_PERIPHERAL_DISPLAY_DEBUG)
+static lv_obj_t *dbg_label;
+static uint32_t recv_count;
+#endif
+
 static void set_link_visible(bool linked) {
     if (linked) {
         lv_obj_add_flag(no_link_label, LV_OBJ_FLAG_HIDDEN);
@@ -111,6 +116,17 @@ static void poll_shadow(lv_timer_t *t) {
         had_link = true;
         set_link_visible(true);
     }
+
+#if IS_ENABLED(CONFIG_ZMK_PERIPHERAL_DISPLAY_DEBUG)
+    recv_count++;
+    lv_label_set_text_fmt(dbg_label, "L%u %u%% F%02x M%02x W%u #%u",
+                          (unsigned)s.data.active_layer,
+                          (unsigned)s.data.battery_level,
+                          (unsigned)s.data.status_flags,
+                          (unsigned)s.data.modifier_flags,
+                          (unsigned)s.data.wpm_value,
+                          (unsigned)recv_count);
+#endif
 
 #if IS_ENABLED(CONFIG_ZMK_PERIPHERAL_DISPLAY_WIDGET_LAYER)
     zmk_widget_peripheral_layer_status_update(&layer_w, &s.data);
@@ -195,6 +211,14 @@ int peripheral_display_init(lv_obj_t *parent) {
     lv_obj_set_style_text_font(no_link_label, &lv_font_unscii_8, 0);
     /* Offset above the center layer label so the two don't overlap. */
     lv_obj_align(no_link_label, LV_ALIGN_CENTER, 0, -24);
+
+#if IS_ENABLED(CONFIG_ZMK_PERIPHERAL_DISPLAY_DEBUG)
+    dbg_label = lv_label_create(parent);
+    lv_label_set_text(dbg_label, "---");
+    lv_obj_set_style_text_font(dbg_label, &lv_font_unscii_8, 0);
+    /* Bottom-left, out of the way of the bongo cat / WPM (bottom-right). */
+    lv_obj_align(dbg_label, LV_ALIGN_BOTTOM_LEFT, 0, -2);
+#endif
     /* Visible initially: shows "NO LINK" until the first status arrives. */
     had_link = false;
     set_link_visible(false);
